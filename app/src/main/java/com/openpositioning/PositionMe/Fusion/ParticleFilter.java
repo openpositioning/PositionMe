045ee478a4a800df61d4cc97e55191e2fe731f59
keeegannn <115988136+keeegannn@users.noreply.github.com>
2026-03-18 09:53:30 +0000

    Added TODO to particle filter file

--- app/src/main/java/com/openpositioning/PositionMe/Fusion/ParticleFilter.java
+++ app/src/main/java/com/openpositioning/PositionMe/Fusion/ParticleFilter.java
@@ -132,7 +132,7 @@ public class ParticleFilter {
 
     /**
      * Update step: assigns weights to particles based on observation likelihood.
-     *
+     * -TODO
      * @return the maximum particle weight after normalisation.
      */
     public double updateWeights() {
